84ef60f365def024572c4344646bc53f8f3f1d43
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-05-19 22:12:02 +1000

    add ncurses work with console

--- main.cpp
+++ main.cpp
@@ -4,18 +4,36 @@
 #include "map.hpp"
 
 int main(int argc, const char * argv[]) {
+    int q = -1;
     Snake actor = *new Snake(3, Point(5, 5));
     Map map = *new Map(20);
     Apple apple = *new Apple();
-//    while (!actor.isDead()) {
+    
+    initscr();
+    noecho();
+    
+    erase();
+    
+    map.clearMap();
+    map.drawSnake(actor._positions);
+    
+    while (!actor.isDead()) {
+        q = -1;
+        erase();
+        
         map.clearMap();
         map.drawSnake(actor._positions);
+
         if (apple.isEat()) {
             apple.setApple(map.mapData, map.fieldLength());
         }
         map.drawApple(apple._position);
-        map.drawMap(cout);
-//    }
+        map.drawMap();
+        q = getch();
+        Point way = map.getWay(q);
+        actor.move(way, map.getElement(way, actor._positions[actor.length()]));
+    }
     
+    endwin();
     return 0;
 }
