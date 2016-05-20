525e95e3c70a6a2273562d1b799f743ad8ec2c5f
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-05-21 01:36:28 +1000

    refactor main

--- main.cpp
+++ main.cpp
@@ -6,18 +6,19 @@
 
 Point getWayPoint(int aKey) {
     switch (aKey) {
-        case 'w': return Point(0, -1);
-        case 'a': return Point(-1, 0);
-        case 's': return Point(0, 1);
-        case 'd': return Point(1, 0);
-        default: return Point(0, 0);
+        case 'w': return Point( 0, -1);
+        case 'a': return Point(-1,  0);
+        case 's': return Point( 0,  1);
+        case 'd': return Point( 1,  0);
+        default:  return Point( 0,  0);
     }
 }
 
 int main(int argc, const char * argv[]) {
     int q = -1;
+    
     Snake actor = *new Snake(3, Point(5, 5));
-    Map map = *new Map(20);
+    Map   map   = *new Map(20);
     Apple apple = *new Apple();
     
     initscr();
@@ -25,28 +26,30 @@ int main(int argc, const char * argv[]) {
     
     erase();
     
-    map.clearMap();
-    map.drawSnake(actor._positions);
-    
     while (!actor.isDead()) {
         q = -1;
+        
         erase();
         
         map.clearMap();
         map.drawSnake(actor._positions);
 
-        if (apple.isEat()) {
+        if (apple._isEat) {
             apple.setApple(map.mapData, map.fieldLength());
         }
+        
         map.drawApple(apple._position);
         map.drawMap();
         
         q = getch();
-        Point userWay = getWayPoint(q);
+        
+        Point userWay    = getWayPoint(q);
         char  mapElement = map.findElement(actor._positions[actor.length() - 1], userWay);
+        
         actor.move(userWay, mapElement, &apple._isEat);
     }
     
     endwin();
+    
     return 0;
 }
