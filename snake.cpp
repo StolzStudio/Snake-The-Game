54329cc6f997bceb2444e3e2d5a7d7e8dad578a6
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-05-21 01:27:52 +1000

    refactor snake module

--- snake.cpp
+++ snake.cpp
@@ -2,6 +2,7 @@
 #include "snake.hpp"
 #include <random>
 
+
 int Snake::length() {
     return this->_length;
 }
@@ -12,22 +13,22 @@ bool Snake::isDead() {
 
 void Snake::move(Point aUserWay, char aMapElement, bool *appleIsEat) {
     for (int i = 0; i < _length - 1; i++) {
-        _positions[i] = _positions[i+1];
+        this->_positions[i] = this->_positions[i+1];
     }
     
     switch (aMapElement) {
         case CHAR_EMPTY: {
-                            _positions[_length - 1] = _positions[_length - 1] + aUserWay;
+                            this->_positions[_length - 1] = this->_positions[_length - 1] + aUserWay;
                             break;
                          }
         case CHAR_BLOCK: {
-                            _isDead = true;
+                            this->_isDead = true;
                             break;
                          }
         case CHAR_APPLE: {
-                            _positions.insert(_positions.end(), _positions[_length - 1] + aUserWay);
+                            this->_positions.insert(this->_positions.end(), this->_positions[_length - 1] + aUserWay);
                             *appleIsEat = true;
-                            _length++;
+                            this->_length++;
                             break;
                          }
     }
@@ -35,14 +36,18 @@ void Snake::move(Point aUserWay, char aMapElement, bool *appleIsEat) {
 }
 
 void Apple::setApple(char **aMap, int aLength) {
-    int y = random() % (aLength - 1);
-    int x = random() % (aLength - 1);
-    if (aMap[y][x] != CHAR_BLOCK) {
-        this->_position = Point(x, y);
+    this->_changePos = false;
+    int x = 0;
+    int y = 0;
+    
+    while (!this->_changePos) {
+        x = random() % (aLength - 1);
+        y = random() % (aLength - 1);
+        
+        if (aMap[y][x] != CHAR_BLOCK) {
+            this->_position = Point(x, y);
+            this->_changePos = true;
+        }
     }
     this->_isEat = false;
-}
-
-bool Apple::isEat() {
-    return this->_isEat;
 }
--- snake.hpp
+++ snake.hpp
@@ -10,12 +10,15 @@ using namespace std;
 
 class Snake {
 protected:
-    int _length;
+    int  _length;
     bool _isDead;
 public:
     vector<Point> _positions;
     
-    Snake(int aLength, Point aPos): _isDead(false), _length(aLength), _positions(_length) {
+    Snake(int aLength, Point aPos): _isDead(false),
+                                    _length(aLength),
+                                    _positions(_length)
+    {
         for (int i = 0; i < _length; i++) {
             _positions[i] = Point((aPos.x() - _length + 1) + i, aPos.y());
         }
@@ -23,20 +26,23 @@ public:
     
     ~Snake() {};
     
-    int length();
+    int  length();
     bool isDead();
     void move(Point aUserWay, char aMapElement, bool *appleIsEat);
 };
 
 class Apple {
+private:
+    bool _changePos;
 public:
     Point _position;
-    bool _isEat;
+    bool  _isEat;
     
     Apple(): _isEat(true) {};
     ~Apple() {};
     
     void setApple(char **aMap, int aLength);
-    bool isEat();
+    
 };
+
 #endif /* snake_hpp */
