35af3b7755911f07db7c9020d5eae5938eea4505
Nicholas Glushchenko <ruscoolman@gmail.com>
2017-01-31 10:01:16 +1000

    add description

---
+++ game.cpp
@@ -0,0 +1,9 @@
+//
+//  game.cpp
+//  snake
+//
+//  Created by Nicholas on 24.05.16.
+//  Copyright © 2016 Nicholas. All rights reserved.
+//
+
+#include "game.hpp"
---
+++ game.hpp
@@ -0,0 +1,14 @@
+//
+//  game.hpp
+//  snake
+//
+//  Created by Nicholas on 24.05.16.
+//  Copyright © 2016 Nicholas. All rights reserved.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <stdio.h>
+
+#endif /* game_hpp */
